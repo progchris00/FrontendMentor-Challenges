86d8dd8be34a813a2da38b8a6e6168ee61f12f0d
progchris00 <prog.christian07@gmail.com>
2024-06-08 20:18:13 +0800

    initial commit of react app template

---
+++ base-apparel-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        We're coming soon Hello fellow shoppers! We're currently building our
+        new fashion store. Add your email below to stay up-to-date with
+        announcements and our launch deals. Email Address
+      </div>
+    </>
+  );
+}
+
+export default App;
