6571d872477d0ddc5305ddb74eeebd8813dfeaee
progchris00 <prog.christian07@gmail.com>
2024-06-09 08:48:57 +0800

    add: create HTML and root variables on CSS

--- base-apparel-react-app/src/App.jsx
+++ base-apparel-react-app/src/App.jsx
@@ -3,12 +3,36 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        We're coming soon Hello fellow shoppers! We're currently building our
-        new fashion store. Add your email below to stay up-to-date with
-        announcements and our launch deals. Email Address
-      </div>
+      <main>
+        <TextSection />
+        <ImageSection />
+      </main>
     </>
+  );
+}
+
+function TextSection() {
+  return (
+    <section className="left-section">
+      <img src="/logo.svg" alt="" />
+      <div className="text">
+        <h1>We're coming soon</h1>
+        <p className="description">
+          Hello fellow shoppers! We're currently building our new fashion store.
+          Add your email below to stay up-to-date with announcements and our
+          launch deals
+        </p>
+        <input type="text" placeholder="Email Address" />
+      </div>
+    </section>
+  );
+}
+
+function ImageSection() {
+  return (
+    <section className="right-section">
+      <img src="/hero-desktop.jpg" alt="" />
+    </section>
   );
 }
 
