34e1dceb9d0e49a3dcdf9bca18104884ab4da756
progchris00 <prog.christian07@gmail.com>
2024-07-09 20:52:30 +0800

    feat: style buttons

--- SocialLinks/tailwind.config.js
+++ SocialLinks/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "hsl(75, 94%, 57%)",
+        white: "hsl(0, 0%, 100%)",
+        grey: "hsl(0, 0%, 20%)",
+        "dark-grey": "hsl(0, 0%, 12%)",
+        "off-black": "hsl(0, 0%, 8%)",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
